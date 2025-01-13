fa484a74f2c6dc825a429e7110e1dd6a11121995
Chamuditha <lilan.2019660@iit.ac.lk>
2025-01-13 08:30:38 +0530

    linked list node

---
+++ linked-list/Node.java
@@ -0,0 +1,16 @@
+public class Node {
+
+    Node next, prev;
+    int data;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+
+    @Override
+    public String toString() {
+        return   "d" + this.data ;
+    }
+}
