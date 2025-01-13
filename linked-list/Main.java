a1de905e2fbd3721e89f1ce278866dc0e922cda0
Chamuditha <lilan.2019660@iit.ac.lk>
2025-01-13 08:32:03 +0530

    main class

---
+++ linked-list/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        MyList list = new MyList();
+
+        list.insert(4);
+        list.insert(6);
+        list.insert(9);
+
+        System.out.println(list);
+
+
+
+
+    }
+}
